3cb7a69e5a0e4cd6e784be1c5dc1b73441a56560
Elizabeth MacCabe <2macce32@solent.ac.uk>
2018-03-19 15:20:20 +0000

    Created Browser Sync task

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,23 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
+//Task to compile SCSS to CSS
 gulp.task('sass', function()
 {
     return gulp.src('site/scss/**/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('site/css'))
+});
+
+//Task to run a server with Browser Sync
+gulp.task('browserSync', function()
+{
+    browserSync.init({
+        server: {
+            baseDir: 'site'
+        },
+    })
 });
 
 //Watch task
