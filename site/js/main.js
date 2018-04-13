e51ec65383088325b7545e170fd86d1736046055
Elizabeth MacCabe <2macce32@solent.ac.uk>
2018-04-13 15:07:12 +0100

    Added a map to the contact map

--- site/js/main.js
+++ site/js/main.js
@@ -9,3 +9,23 @@ $(document).ready(function()
 {
     $('.collapsible').collapsible();
 });
+
+//Google Maps on Contact Page
+function map()
+{
+    var center = new google.maps.LatLng(50.9188, -1.3952)
+    
+    var mapProp = 
+    {
+        center: center,
+        zoom: 14,
+    };
+    
+    var map = new google.maps.Map(document.getElementById("map"), mapProp);
+    var marker = new google.maps.Marker
+    ({
+        position: center,
+    });
+    
+    marker.setMap(map);
+}
